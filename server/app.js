959fd54e9dc334ee0b7b47f6d30f000a762b613e
premlama12 <lprem0418@gmail.com>
2024-07-04 12:00:03 -0500

    todey

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
 const http=require("http");
 let server=http.createServer((req,res)=>{
-    res.end(<h1>Hello world</h1>);
-})
+    res.end(`<h1>Hello world....</h1>`);
+});
 server.listen(8080,'127.0.0.1',(err)=>{
     if(err) throw err
     console.log("Server started")
-})
+});
